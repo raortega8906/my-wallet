deae42c5de7361ca93adba1a8592e7f69e92797f
Rafael A. Ortega <raortega8906@gmail.com>
2025-02-17 17:52:52 +0100

    Create seeder for test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Rafael A. Ortega',
+            'email' => 'raortega8906@gmail.com',
+            'password' => Hash::make('laravel2024.'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
