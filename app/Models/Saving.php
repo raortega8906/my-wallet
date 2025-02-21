558058d3515c5bb7a51759f0bdc8e577a328df2f
Rafael A. Ortega <raortega8906@gmail.com>
2025-02-21 16:17:25 +0100

    Add factories for Deadline, Expense, ExtraIncome, Saving, and TargetBalance

--- app/Models/Saving.php
+++ app/Models/Saving.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Saving extends Model
 {
+    use HasFactory;
+    
     protected $fillable = [
         'amount',
         'user_id',
--- app/Models/TargetBalance.php
+++ app/Models/TargetBalance.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TargetBalance extends Model
 {
+    use HasFactory;
+    
     protected $fillable = [
         'target_balance',
         'user_id',
