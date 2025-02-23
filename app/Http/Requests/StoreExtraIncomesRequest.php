432581867f9ff35bd564e2d86c39cb712efe07cb
Rafael A. Ortega <raortega8906@gmail.com>
2025-02-23 19:35:13 +0100

    Add Store and Update ExtraIncomes Request

--- app/Http/Requests/StoreExtraIncomesRequest.php
+++ app/Http/Requests/StoreExtraIncomesRequest.php
@@ -11,7 +11,7 @@ class StoreExtraIncomesRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class StoreExtraIncomesRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+            'amount' => 'required|numeric|min:0',
         ];
     }
 }
--- app/Http/Requests/UpdateExtraIncomeRequest.php
+++ app/Http/Requests/UpdateExtraIncomeRequest.php
@@ -11,7 +11,7 @@ class UpdateExtraIncomeRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UpdateExtraIncomeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+            'amount' => 'required|numeric|min:0',
         ];
     }
 }
